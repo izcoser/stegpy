45f1970891bda6e58f899ec15711041f1ee0b2bf
someone <someone@somepl14513aceeeeeeeeeeeee.com>
2018-08-27 15:52:31 -0300

    Added message terminator sequence

--- lsb.py
+++ lsb.py
@@ -4,31 +4,27 @@ import numpy
 import codecs
 from PIL import Image
 
-def is_allowed(character):
-    symbols = (['?', '!', '@', "#", '$', '%', '^', '&', '*', '(', ')', '.', '-', ' ', ',', ':', ';', '/', '\\', '\n'])
-    return character.isalpha() or character in symbols
-
-def get_image(image_path):
-    """Get a numpy array of an image so that one can access values[x][y]."""
+''' Returns a numpy array of an image so that one can access values[x][y]. '''
+def getImage(image_path):
     image = Image.open(image_path, 'r')
     width, height = image.size
     pixel_values = list(image.getdata())
     if image.mode == 'RGB':
         channels = 3
-    elif image.mode == 'L':
-        channels = 1
     else:
-        print("Unknown mode: %s" % image.mode)
-        return None
+        print("Image mode not supported yet: %s\nAborting." % image.mode)
+        exit(1)
     pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
     return pixel_values
 
+''' Returns the message as a list of binaries. '''
 def getMessageBinaries(message):
     binaries = []
     for character in message:
         binaries.append(bin(ord(character))[2:].zfill(8))
     return binaries
 
+''' Returns the pixels as a list of binaries. '''
 def getPixelsBinaries(pixels):
     binaries = []
     for pixel_column in pixels:
@@ -37,6 +33,7 @@ def getPixelsBinaries(pixels):
                 binaries.append(bin(color)[2:].zfill(8))
     return binaries
 
+''' Returns the pixels with encoded message and terminator zeroes as a list of binaries. '''
 def changeLeastSignificantBit(pixels_binaries, message_binaries):
     i = 0
     new_pixels_binaries = []
@@ -50,12 +47,17 @@ def changeLeastSignificantBit(pixels_binaries, message_binaries):
             new_pixels_binaries[i][7] = binary[j + 1]
             i += 1
 
+    k = i
+    for k in range(i, i + 100):
+        new_pixels_binaries[k][6] = '0'
+        new_pixels_binaries[k][7] = '0'
+
     return new_pixels_binaries
 
 
-
+''' Creates a similar image with the encoded message. '''
 def insertMessage(message, image_path):
-    pixels = get_image(image_path)
+    pixels = getImage(image_path)
 
     number_of_pixels = len(pixels) * len(pixels[0])
     number_of_characters = len(message)
@@ -92,12 +94,12 @@ def insertMessage(message, image_path):
     new_pixels = list(tuple(x) for x in new_pixels)
     im = Image.new('RGB', (len(pixels), len(pixels[0])))
     im.putdata(new_pixels)
-    im.save(image_path + '.png') #Can't seem to insert messages if I save this as .jpg
+    im.save('steg_' + image_path, 'PNG')
 
 
-
+''' Reads inserted message. '''
 def readInsertedMessage(image_path, write_to_file = 0):
-    pixels = get_image(image_path)
+    pixels = getImage(image_path)
     pixels_binaries = getPixelsBinaries(pixels)
 
     values = []
@@ -106,20 +108,28 @@ def readInsertedMessage(image_path, write_to_file = 0):
 
     read_message = []
     i = 0
+    terminator_count = 0
     binary = []
+
     for value in values:
+        if(value == '00'):
+            terminator_count += 1
+            if(terminator_count == 100):
+                break
+        else:
+            terminator_count = 0
+
         i += 1
         binary += value
         if(i == 4):
             character = chr(int(''.join(binary), 2))
             i = 0
             binary = []
-            if(is_allowed(character)):
-                read_message.append(character)
+            read_message.append(character)
 
     if(write_to_file):
         with codecs.open(image_path + ".txt", "w", 'utf-8-sig') as text_file:
-            print(''.join(read_message), file=text_file)
+            print(''.join(read_message[:-24]), file=text_file)
         print("Information written to " + image_path + ".txt")
     else:
-        print(''.join(read_message))
+        print(''.join(read_message[:-24]))
