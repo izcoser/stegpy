cfbb3696392586efcde94983a462bbf033bcc36a
someone <someone@somepl14513aceeeeeeeeeeeee.com>
2018-09-27 19:15:06 -0300

    Allow user to choose encoding option

--- steg.py
+++ steg.py
@@ -6,19 +6,23 @@ import time
 def start_message():
     print('''
     steg.py v2
-        Usage: steg.py <command> <switch> <information> <image_name> <switch>
+        Usage: python3 steg.py <command> <switch> <information> <image_name> <switch> <switch>
+        Example: python3 steg.py write -f archive.7z image.png -p -b=4
 
 <Commands>
   write: Add information to image
   read: Read information from image
 <Switches>
+  These are optional.
   -f: File input
+  -b: Bits to encode per byte (1, 2 or 4. Default is 2)
   -p: Set password
     ''')
 
 def main():
     password = None
     filename = None
+    bits = 2
 
     if(len(sys.argv) == 1):
         start_message()
@@ -35,24 +39,23 @@ def main():
             else:
                 message = sys.argv[2].encode('utf-8')
                 image_path = sys.argv[3]
-            if(sys.argv[-1] == '-p'):
+            if('-p' in sys.argv):
                 while True:
                     password = getpass('Enter password (will not be echoed) :')
                     password_2 = getpass('Verify password (will not be echoed) :')
                     if password == password_2:
                         break;
+            if('-b=1' in sys.argv):
+                bits = 1
+            elif('-b=4' in sys.argv):
+                bits = 4
 
-            insert_message(message, image_path, filename, password)
+            insert_message(message, image_path, bits, filename, password)
 
         elif(sys.argv[1] == 'read'):
             image_path = sys.argv[2]
-            if(sys.argv[-1] == '-p'):
-                while True:
-                    password = getpass('Enter password (will not be echoed) :')
-                    password_2 = getpass('Verify password (will not be echoed) :')
-                    if password == password_2:
-                        break;
-
+            if('-p' in sys.argv):
+                password = getpass('Enter password (will not be echoed) :')
             read_message(image_path, password)
 
 if __name__== "__main__":
