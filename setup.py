c69073d40537d71a05a0afffa61e0940999a8ed5
dhsdshdhk <28062763+dhsdshdhk@users.noreply.github.com>
2022-08-30 00:01:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="stegpy",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="dhsdshdhk",
+    description="A program for encoding information in image and audio files through steganography.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/dhsdshdhk/stegpy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
